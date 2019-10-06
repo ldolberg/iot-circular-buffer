66ad1c8489331d09aaa08fcba97dfe5737b1ed11
ldolberg <lautaro.dolberg@gmail.com>
2019-10-06 01:12:20 +0000

    library

--- main.c
+++ main.c
@@ -3,53 +3,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "cbuf.h"
 
-typedef struct circular_buf_t Queue;
-
-struct circular_buf_t {
-    uint8_t *buffer;
-    size_t head;
-    size_t tail;
-    size_t size;
-};
-
-void init(Queue *q, size_t size){
-  q->buffer = malloc(sizeof(uint8_t) * size);  
-}
-
-void add_elem(Queue *cbuf, uint8_t data){
-  if (cbuf->head == 49){
-    cbuf->head = 0;
-  }
-
-  cbuf->buffer[cbuf->head] = data;
-  cbuf->head += 1;
-  if(cbuf->head == cbuf->tail){
-    cbuf->tail += 1;
-  }
-  cbuf->size+=1;
-}
-
-size_t read(Queue *cbuf){
-  size_t res = cbuf->buffer[cbuf->tail];
-  if (cbuf->tail == 49){
-    cbuf->tail = 0;
-  }else{
-    cbuf->tail +=1;
-  }
-  cbuf->size-=1;
-  return res;
-}
-
-size_t average(Queue *cbuf){
-  size_t val = 0;
-  int n = 0;
-  while(cbuf->size > 0){
-    val += read(cbuf);
-    n++;
-  }
-  return val/n;
-}
 
 int main(void) {
   printf("Hello World\n");
